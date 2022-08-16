160b499c0ef0571c32333dba643435a098525f5e
Nirmal Patel <patelnirmal17592@gmail.com>
2022-08-16 13:25:36 +1200

    before I make API calls

--- src/components/findACar/FindACar.jsx
+++ src/components/findACar/FindACar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import styles from './FindACar.module.css'
+import React from "react";
+import styles from "./FindACar.module.css";
 
 export const FindACar = () => {
   return (
-    <div className={styles.hello}>Hello world</div>
-  )
-}
+    <div className={styles.search_container}>
+      <div className={styles.search_btn}>Search</div>
+      <div className={styles.search_content}></div>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FindACar } from "../components/findACar/FindACar.jsx";
 
 const Home = () => {
   return (
     <>
-      <Link to='/'><FindACar /></Link>
+      <FindACar />
     </>
   );
 };
